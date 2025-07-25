516203498245dd40250d7af8aab5d3d9eaf0bef3
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-07-25 19:17:46 +0530

    Time: 4 ms (16.96%), Space: 9.9 MB (79.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
+    // bool isPalindrome(string s) {
+    //     string ans="";
+    //     for(auto c:s)
+    //     {
+    //         if(isalnum(c))
+    //         {
+    //             ans+=tolower(c);
+    //         }
+    //     }
+    //     string ansr=ans;
+    //     reverse(ansr.begin(),ansr.end());
+    //     return ans==ansr;
+
+    // }
+
+
+
     bool isPalindrome(string s) {
-        string ans="";
-        for(auto c:s)
-        {
-            if(isalnum(c))
-            {
-                ans+=tolower(c);
-            }
-        }
-        string ansr=ans;
-        reverse(ansr.begin(),ansr.end());
-        return ans==ansr;
+    int left = 0, right = s.size() - 1;
+
+    while (left < right) {
+        // Move left forward if not alphanumeric
+        while (left < right && !isalnum(s[left])) left++;
+
+        // Move right backward if not alphanumeric
+        while (left < right && !isalnum(s[right])) right--;
+
+        // Compare characters in lower case
+        if (tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        left++;
+        right--;
     }
+
+    return true;
+}
+
 };
