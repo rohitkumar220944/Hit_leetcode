b018aba46b81d9012dc5accba9a6573451952868
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-09-01 12:03:25 +0530

    Time: 3 ms (66.39%), Space: 8.4 MB (11.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n<=1)
-        {
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
-    }
-};
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<=1)
+//         {
+//             return n;
+//         }
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
 
 
 // class Solution {
@@ -25,3 +25,21 @@ public:
 //         return helper(n, dp);
 //     }
 // };
+
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) return n;
+
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0, dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+
