e85c40e1819f685fa9c51bbd7838ee8f81897685
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-09-11 11:14:55 +0530

    Time: 7 ms (73.45%), Space: 19.5 MB (79.33%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,15 +1,28 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         if (nums.size() < 3) return nums.size();
+//         int i = 2;
+//         while (i < nums.size()) {
+//             if (nums[i] == nums[i - 1] && nums[i] == nums[i - 2]) {
+//                 nums.erase(nums.begin() + i);
+//             } else {
+//                 i++;
+//             }
+//         }
+//         return nums.size();
+//     }
+// };
+
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.size() < 3) return nums.size();
-        int i = 2;
-        while (i < nums.size()) {
-            if (nums[i] == nums[i - 1] && nums[i] == nums[i - 2]) {
-                nums.erase(nums.begin() + i);
-            } else {
-                i++;
+    int removeDuplicates(std::vector<int>& nums) {
+        int j = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (j == 1 || nums[i] != nums[j - 2]) {
+                nums[j++] = nums[i];
             }
         }
-        return nums.size();
+        return j;
     }
 };
