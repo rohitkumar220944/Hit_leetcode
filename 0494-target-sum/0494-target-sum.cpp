755ab6a2224da80b5a6a8a35b4a49ef37aef5de4
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-09-15 19:47:50 +0530

    Time: 151 ms (36.08%), Space: 59.4 MB (10.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int f(int ind, vector<int>& arr, int sum)
+    int f(int ind, vector<int>& arr, int sum, vector<unordered_map<int,int>> &dp)
     {
       if (ind < 0) {
            if (sum == 0) return 1;
             else return 0;
        }
-        int add=f(ind-1, arr, sum+arr[ind]);
-        
-        int sub=f(ind-1, arr, sum-arr[ind]);
 
-        return add+sub;
+       if(dp[ind].count(sum))return dp[ind][sum];
+
+        int add=f(ind-1, arr, sum+arr[ind], dp);
+        
+        int sub=f(ind-1, arr, sum-arr[ind], dp);
+
+        return dp[ind][sum]= add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        return f(n-1, nums, target);
+        vector<unordered_map<int,int>>dp(n);
+        return f(n-1, nums, target, dp);
     }
 };
