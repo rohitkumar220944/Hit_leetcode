9016ed9b6ef4da65fb7e14fd88eb5ba8e53c04c0
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-09-18 17:54:40 +0530

    Time: 19 ms (55.82%), Space: 28.4 MB (55.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -21,3 +21,29 @@ public:
         return f(amount,coins,n-1,dp);
     }
 };
+
+
+// class Solution {
+// public:
+//     int change(int amount, vector<int>& coins) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+
+//         // Base case: only using coins[0]
+//         for(int tar=0; tar<=amount; tar++) {
+//             if(tar % coins[0] == 0) dp[0][tar] = 1;
+//         }
+
+//         for(int ind=1; ind<n; ind++) {
+//             for(int tar=0; tar<=amount; tar++) {
+//                 int notTake = dp[ind-1][tar];
+//                 int Take = 0;
+//                 if(coins[ind] <= tar) Take = dp[ind][tar - coins[ind]];
+
+//                 dp[ind][tar] = notTake + Take;
+//             }
+//         }
+
+//         return dp[n-1][amount];
+//     }
+// };
