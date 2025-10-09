0e31aa8e8ca45800e1762e6d541a9d956d4e26f4
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-10-09 12:16:00 +0530

    Time: 3 ms (89.92%), Space: 11 MB (57.7%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -23,27 +23,50 @@
 // };
 
 
+// class Solution {
+// public:
+//     int numberOfSubstrings(string s) {
+//         int i = 0;
+//         int ans = 0;
+//         int n = s.size();
+//         unordered_map <char,int>mp;
+
+//         for(int j = 0; j < n ; j++){
+//             //calculation
+//             mp[s[j]]++;
+//             //shrink the window 
+//             while(mp.size() == 3){
+//                 ans += n - j;
+//                 mp[s[i]]--;
+//                 if(mp[s[i]] == 0){
+//                     mp.erase(s[i]);
+//                 }
+//                 i++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int i = 0;
-        int ans = 0;
         int n = s.size();
-        unordered_map <char,int>mp;
+        int left = 0;
+        int result = 0;
+        int arr[3] = {0, 0, 0};
 
-        for(int j = 0; j < n ; j++){
-            //calculation
-            mp[s[j]]++;
-            //shrink the window 
-            while(mp.size() == 3){
-                ans += n - j;
-                mp[s[i]]--;
-                if(mp[s[i]] == 0){
-                    mp.erase(s[i]);
-                }
-                i++;
+        for (int right = 0; right < n; right++) {
+            arr[s[right] - 'a']++;
+
+            // When all a,b,c are present, shrink from the left
+            while (arr[0] > 0 && arr[1] > 0 && arr[2] > 0) {
+                result += (n - right);  // all substrings from [left...right] to end are valid
+                arr[s[left] - 'a']--;
+                left++;
             }
         }
-        return ans;
+
+        return result;
     }
 };
