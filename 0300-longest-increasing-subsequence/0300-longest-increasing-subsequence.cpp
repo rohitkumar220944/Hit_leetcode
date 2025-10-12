9c3358aef52f4d3826c33b59785b65a8b1f0421f
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-10-12 20:11:59 +0530

    Time: 538 ms (23.4%), Space: 296.5 MB (13.28%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int f(int ind, int prev, int n, vector<int>& nums, vector<vector<int>> &dp)
+    {
+        if(ind==n)return 0;
+        if(dp[ind][prev+1]!=-1)return dp[ind][prev+1];
+
+        int npick =0+f(ind+1,prev,n,nums,dp);
+        int pick=0;
+        if(prev==-1 || nums[ind]>nums[prev])
+         pick=1+f(ind+1,ind,n,nums,dp);
+
+        return dp[ind][prev+1]=max(npick,pick);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+    int n=nums.size();   
+    vector<vector<int>>dp(n,vector<int>(n+1,-1));
+    return f(0,-1,n,nums,dp);
+
+    }
+};
