bbb8bfe83a248809ba0fa439576f7b6020fc77db
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-12-11 09:45:49 +0530

    Time: 99 ms (8.84%), Space: 35.4 MB (5.31%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,55 +6,77 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode*a=headA;
+//         ListNode*b=headB;
+//         while(a->next!=NULL&&b->next!=NULL)
+//         {
+//             if(a==b)
+//             {
+//                 return a;
+//             }
+//             a=a->next;
+//             b=b->next;
+//         }
+
+//         if(a->next==0)
+//         {
+//             int blen=0;
+//             while(b->next!=NULL)
+//             {
+//                 blen++;
+//                 b=b->next;
+//             }
+
+//             while(blen--)
+//             {
+//                 headB=headB->next;
+//             }
+//         }
+
+//         else{
+//               int alen=0;
+//               while(a->next!=NULL)
+//               {
+//                 alen++;
+//                 a=a->next;
+//               }
+//               while(alen--)
+//               {
+//                 headA=headA->next;
+//               }
+//         }
+        
+//         while( headA!=headB)
+//         {
+//             headA=headA->next;
+//             headB=headB->next;
+//         }
+//         return headA ;
+
+//     }
+// };
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode*a=headA;
         ListNode*b=headB;
-        while(a->next!=NULL&&b->next!=NULL)
-        {
-            if(a==b)
-            {
-                return a;
-            }
-            a=a->next;
-            b=b->next;
-        }
-
-        if(a->next==0)
-        {
-            int blen=0;
-            while(b->next!=NULL)
-            {
-                blen++;
-                b=b->next;
-            }
-
-            while(blen--)
-            {
-                headB=headB->next;
-            }
-        }
-
-        else{
-              int alen=0;
-              while(a->next!=NULL)
-              {
-                alen++;
-                a=a->next;
-              }
-              while(alen--)
-              {
-                headA=headA->next;
-              }
-        }
-        
-        while( headA!=headB)
-        {
-            headA=headA->next;
-            headB=headB->next;
-        }
-        return headA ;
-
+       map<ListNode*, int>mp;
+       while(a!=NULL)
+       {
+        mp[a]=1;
+        a=a->next;
+       }
+       while(b!=NULL)
+       {
+        if(mp[b]==1)
+        return b;
+        b=b->next;
+       }
+       return NULL;
     }
 };
