bee9864028527c2c1318ebdf70a81cd87613b192
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2026-01-10 00:47:12 +0530

    Time: 0 ms (100%), Space: 53 MB (22.14%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,23 +1,51 @@
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         int n=nums.length;
+//         if(n==1)return nums[0];
+//        for(int i=0;i<n;i++)
+//        {
+//         if(i==0)
+//         {
+//             if(nums[i]!=nums[i+1])return nums[i];
+//         }
+//         else if(i==n-1)
+//         {
+//             if(nums[i]!=nums[i-1])return nums[i];
+//         }
+         
+//         else if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1])
+//         {
+//             return nums[i];
+//         }
+//        } 
+//        return -1;
+//     }
+// }
+
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n=nums.length;
         if(n==1)return nums[0];
-       for(int i=0;i<n;i++)
-       {
-        if(i==0)
+        int s=1;
+        int e=n-2;
+        if(nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        while(s<=e)
         {
-            if(nums[i]!=nums[i+1])return nums[i];
+            int mid=s+(e-s)/2;
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1])
+            {
+                return nums[mid];
+            }
+            else if(mid%2==1 && nums[mid]==nums[mid-1] || mid%2==0 && nums[mid]==nums[mid+1])
+            {
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
         }
-        else if(i==n-1)
-        {
-            if(nums[i]!=nums[i-1])return nums[i];
-        }
-         
-        else if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1])
-        {
-            return nums[i];
-        }
-       } 
-       return -1;
+        return -1;
     }
 }
