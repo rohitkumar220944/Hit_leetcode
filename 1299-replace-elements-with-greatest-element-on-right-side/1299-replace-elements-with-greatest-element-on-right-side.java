15dda7fb60a75e7d4e371e078fb03653594365b6
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2026-04-11 00:25:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,20 +1,41 @@
+// class Solution {
+//     public int[] replaceElements(int[] arr) {
+//         int n=arr.length;
+//         int []ans=new int[n];
+//         int idx=0;
+       
+//         for(int i=0;i<n;i++)
+//         {
+//             int maxi=-1;
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 maxi=Math.max(maxi,arr[j]);
+
+//             }
+//             ans[i]=maxi;
+            
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int[] replaceElements(int[] arr) {
         int n=arr.length;
-        int []ans=new int[n];
-        int idx=0;
-       
-        for(int i=0;i<n;i++)
+       int maxi=-1;
+       for(int i=n-1;i>=0;i--)
+       {
+        if(maxi<arr[i])
         {
-            int maxi=-1;
-            for(int j=i+1;j<n;j++)
-            {
-                maxi=Math.max(maxi,arr[j]);
+            int temp=arr[i];
+            arr[i]=maxi;
+            maxi=temp;
+            continue;
 
-            }
-            ans[i]=maxi;
-            
         }
-        return ans;
+        arr[i]=maxi;
+       }
+       return arr;
     }
 }
