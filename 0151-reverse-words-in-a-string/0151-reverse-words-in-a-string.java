46321c24059bbe201f48b68e4bd4dc65746181c7
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2026-05-27 19:16:34 +0530

    Time: 7 ms (64.91%), Space: 44.2 MB (79.04%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,39 +1,61 @@
-import java.util.Stack;
+// import java.util.Stack;
+
+// class Solution {
+//     public String reverseWords(String s) {
+
+//         Stack<String> st = new Stack<>();
+//         StringBuilder temp = new StringBuilder();
+
+//         for (int i = 0; i < s.length(); i++) {
+
+//             // skip spaces
+//             while (i < s.length() && s.charAt(i) == ' ') {
+//                 i++;
+//             }
+
+//             // collect word
+//             while (i < s.length() && s.charAt(i) != ' ') {
+//                 temp.append(s.charAt(i));
+//                 i++;
+//             }
+
+//             if (temp.length() > 0) {
+//                 st.push(temp.toString());
+//                 temp.setLength(0);
+//             }
+//         }
+
+//         StringBuilder ans = new StringBuilder();
+
+//         while (!st.isEmpty()) {
+//             ans.append(st.pop());
+//             if (!st.isEmpty()) {
+//                 ans.append(" ");
+//             }
+//         }
+
+//         return ans.toString();
+//     }
+// }
 
 class Solution {
     public String reverseWords(String s) {
+        
+        String []st=s.trim().split("\\s+");
+        StringBuilder sr=new StringBuilder();
 
-        Stack<String> st = new Stack<>();
-        StringBuilder temp = new StringBuilder();
+        for(int i=st.length-1;i>=0;i--)
+        {
+            sr.append(st[i]);
 
-        for (int i = 0; i < s.length(); i++) {
-
-            // skip spaces
-            while (i < s.length() && s.charAt(i) == ' ') {
-                i++;
+            if(i>0)
+            {
+                sr.append(" ");
             }
 
-            // collect word
-            while (i < s.length() && s.charAt(i) != ' ') {
-                temp.append(s.charAt(i));
-                i++;
-            }
-
-            if (temp.length() > 0) {
-                st.push(temp.toString());
-                temp.setLength(0);
-            }
         }
+        return sr.toString();
 
-        StringBuilder ans = new StringBuilder();
 
-        while (!st.isEmpty()) {
-            ans.append(st.pop());
-            if (!st.isEmpty()) {
-                ans.append(" ");
-            }
-        }
-
-        return ans.toString();
     }
 }
